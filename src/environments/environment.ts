bf08a4547c942b1c3b96e0f491cb98ec16e89814
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-09-23 16:34:41 -0500

    Removed folder from repo and added to .gitignore

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  supabaseUrl: 'https://avpsrhwbtgjelbxctdxb.supabase.co/',
-  supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2cHNyaHdidGdqZWxieGN0ZHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4Nzg3MDEsImV4cCI6MjA0MjQ1NDcwMX0.w8JPe3j0Sm2T0z3l5BWUv9MZojhGsV6QZIW92nJiN4I',
-};
