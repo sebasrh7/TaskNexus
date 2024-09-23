953d39b157c51e314f26009983517fb404e85e05
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-09-23 16:31:05 -0500

    Refactor styles and adjust login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
@@ -19,27 +29,35 @@ import { ThemeComponent } from '../theme/theme.component';
   standalone: true,
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-
+    MatDividerModule,
     ThemeComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
-  email: string = '';
+  emailForm: FormGroup;
   linkSuccess: boolean = false;
+  isLoading: boolean = false;
   error: string = '';
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private snackBar: MatSnackBar
   ) {
+    this.emailForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+
     this.authService.currentUser.subscribe((user) => {
       if (user) {
         this.router.navigateByUrl('/workspace', { replaceUrl: true });
@@ -57,16 +75,33 @@ export class LoginComponent implements OnInit {
   }
 
   async loginWithEmail() {
-    try {
-      const result = await this.authService.loginWithEmail(this.email);
-      if (!result.error) {
-        this.linkSuccess = true;
-        this.error = '';
-      } else {
-        this.error = result.error.message;
+    if (this.isLoading) return; // Evita múltiples clics
+    this.isLoading = true; // Deshabilita el botón
+    const email = this.emailForm.get('email')?.value;
+    if (email) {
+      try {
+        const result = await this.authService.loginWithEmail(email);
+        if (!result.error) {
+          this.linkSuccess = true;
+          this.error = '';
+          this.snackBar.open('Magic link sent! Check your email.', 'Close', {
+            duration: 3000,
+          });
+        } else {
+          this.error = result.error.message;
+
+          this.snackBar.open(`Error: ${this.error}`, 'Close', {
+            duration: 3000,
+          });
+        }
+      } catch (error) {
+        console.error(error);
+        this.snackBar.open('An unexpected error occurred.', 'Close', {
+          duration: 3000,
+        });
+      } finally {
+        this.isLoading = false;
       }
-    } catch (error) {
-      console.error(error);
     }
   }
 
@@ -75,6 +110,17 @@ export class LoginComponent implements OnInit {
       this.authService.loginWithGoogle();
     } catch (error) {
       console.error(error);
+      this.snackBar.open('An unexpected error occurred.', 'Close', {
+        duration: 3000,
+      });
     }
+  }
+
+  getEmailErrorMessage() {
+    const emailControl = this.emailForm.get('email');
+    if (emailControl?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return emailControl?.hasError('email') ? 'Not a valid email' : '';
   }
 }
